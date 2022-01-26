ca5e788525793ffefa2b72e19ec070a38e01c675
fanlia <3093932086@qq.com>
2022-01-26 14:00:51 +0800

    first commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,62 @@
 
 <script>
     import 'bulma/css/bulma.css'
+    import '@fortawesome/fontawesome-free/js/all.js'
 </script>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class="box">
+I'm in a box.
+</div>
+
+<form class="box">
+<div class="field">
+<label class="label">Email</label>
+<div>
+<input class="input" type="email" placeholder="e.g. alex@example.com">
+</div>
+</div>
+<div class="field">
+<label class="label">Password</label>
+<div>
+<input class="input" type="password" placeholder="*******">
+</div>
+</div>
+<button class="button">Sign in</button>
+</form>
+
+<div class="box">
+<article class="media">
+<div class="media-left">
+<figure class="image is-64x64">
+<img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
+</figure>
+</div>
+<div class="media-content">
+<div class="content">
+<p>
+<strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+<br>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+</p>
+</div>
+<nav class="level">
+<div class="level-left">
+<a class="level-item">
+<span class="icon is-small">
+<i class="fas fa-reply"></i>
+</span>
+</a>
+<a class="level-item">
+<span class="icon is-small">
+<i class="fas fa-retweet"></i>
+</span>
+</a>
+<a class="level-item">
+<span class="icon is-small">
+<i class="fas fa-heart"></i>
+</span>
+</a>
+</div>
+</nav>
+</article>
+</div>
